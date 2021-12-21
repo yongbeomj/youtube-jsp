09c6ae2c3d087ebbd897ebfd4ff22ffc7d5957a0
504 <504@DESKTOP-7RTPGGG>
2021-12-21 17:57:30 +0900

    www

---
+++ 장희동/src/main/java/dao/VideoDao.java
@@ -0,0 +1,60 @@
+package dao;
+
+import java.util.ArrayList;
+
+import dto.Video;
+
+public class VideoDao extends DB{
+
+	public VideoDao() {}
+	
+	public static VideoDao videoDao = new VideoDao();
+	public static VideoDao getVideoDao() {
+		return videoDao;
+	}
+	
+	//영상 등록
+	public boolean videoupload(Video video) {
+		String sql = "insert into video(c_no, v_thumbnail, v_name, v_detail) value(?,?,?,?)";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, video.getC_no());
+			preparedStatement.setString(2, video.getV_thumbnail());
+			preparedStatement.setString(3, video.getV_name());
+			preparedStatement.setString(4, video.getV_detail());
+			preparedStatement.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	//영상 목록 출력
+	 
+	
+	//영상 번호에 해당하는 영상 가져오기
+	public Video getvideo(int v_no) {
+		String sql = "select * from video where v_no=?";
+		try {
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, v_no);
+			resultSet = preparedStatement.executeQuery();
+			if(resultSet.next()) {
+				Video video = new Video(
+						resultSet.getInt(1), 
+						resultSet.getInt(2),
+						resultSet.getString(3), 
+						resultSet.getString(4), 
+						resultSet.getString(5), 
+						resultSet.getString(6),  
+						resultSet.getString(7));
+				return video;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
---
+++ 장희동/src/main/java/dto/Video.java
@@ -0,0 +1,86 @@
+package dto;
+
+public class Video {
+	
+	//필드
+	private int v_no;
+	private int c_no;
+	private String v_thumbnail;
+	private String v_name;
+	private String v_detail;
+	private String v_createdate;
+	private String v_updatedate;
+
+	//생성자
+	public Video() {}
+
+	public Video(int v_no, int c_no, String v_thumbnail, String v_name, String v_detail, String v_createdate,
+			String v_updatedate) {
+		this.v_no = v_no;
+		this.c_no = c_no;
+		this.v_thumbnail = v_thumbnail;
+		this.v_name = v_name;
+		this.v_detail = v_detail;
+		this.v_createdate = v_createdate;
+		this.v_updatedate = v_updatedate;
+	}
+
+	public int getV_no() {
+		return v_no;
+	}
+
+	public void setV_no(int v_no) {
+		this.v_no = v_no;
+	}
+
+	public int getC_no() {
+		return c_no;
+	}
+
+	public void setC_no(int c_no) {
+		this.c_no = c_no;
+	}
+
+	public String getV_thumbnail() {
+		return v_thumbnail;
+	}
+
+	public void setV_thumbnail(String v_thumbnail) {
+		this.v_thumbnail = v_thumbnail;
+	}
+
+	public String getV_name() {
+		return v_name;
+	}
+
+	public void setV_name(String v_name) {
+		this.v_name = v_name;
+	}
+
+	public String getV_detail() {
+		return v_detail;
+	}
+
+	public void setV_detail(String v_detail) {
+		this.v_detail = v_detail;
+	}
+
+	public String getV_createdate() {
+		return v_createdate;
+	}
+
+	public void setV_createdate(String v_createdate) {
+		this.v_createdate = v_createdate;
+	}
+
+	public String getV_updatedate() {
+		return v_updatedate;
+	}
+
+	public void setV_updatedate(String v_updatedate) {
+		this.v_updatedate = v_updatedate;
+	}
+	
+	
+	
+}
